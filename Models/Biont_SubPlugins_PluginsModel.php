bfbf057b515ab0dd6ac1a320d3dab9236e5d4c34
Biont <arbelzapf@gmail.com>
2015-02-13 22:56:09 +0100

    Fix for de/activating single plugins

--- Models/Biont_SubPlugins_PluginsModel.php
+++ Models/Biont_SubPlugins_PluginsModel.php
@@ -168,18 +168,18 @@ class Biont_SubPlugins_PluginsModel {
 			/**
 			 * Try to load language files for this plugin
 			 */
-			$data     = get_file_data( $filename, array(
+			$data = get_file_data( $filename, array(
 				'TextDomain' => 'Text Domain',
 				'DomainPath' => 'Domain Path',
 			) );
 
 			if ( isset( $data[ 'TextDomain' ], $data[ 'DomainPath' ] ) ) {
 				$domain = $data[ 'TextDomain' ];
-				$path = dirname($filename).$data['DomainPath'];
+				$path   = dirname( $filename ) . $data[ 'DomainPath' ];
 				$locale = get_locale();
 
 				$mofile = $domain . '-' . $locale . '.mo';
-				load_textdomain( $domain, $path . '/'. $mofile );
+				load_textdomain( $domain, $path . '/' . $mofile );
 			}
 
 			include_once( $filename );
@@ -233,6 +233,9 @@ class Biont_SubPlugins_PluginsModel {
 	public function bulk_activate( $plugins ) {
 
 		if ( ! empty( $plugins ) ) {
+			if ( is_string( $plugins ) ) {
+				$plugins = array( $plugins );
+			}
 			foreach ( $plugins as $plugin ) {
 				$this->activate_plugin( $plugin );
 			}
@@ -242,6 +245,9 @@ class Biont_SubPlugins_PluginsModel {
 	public function bulk_deactivate( $plugins ) {
 
 		if ( ! empty( $plugins ) ) {
+			if ( is_string( $plugins ) ) {
+				$plugins = array( $plugins );
+			}
 			foreach ( $plugins as $plugin ) {
 				$this->deactivate_plugin( $plugin );
 			}
