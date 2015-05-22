b655820241811c365f9536f239627112a14b5626
Biont <arbelzapf@gmail.com>
2015-05-22 16:08:56 +0200

    Add a bunch of actions to the sub-plugin list

--- Views/Biont_SubPlugins_PluginsView.php
+++ Views/Biont_SubPlugins_PluginsView.php
@@ -5,45 +5,59 @@
  * Date: 15.08.2014
  * Time: 11:29
  */
+class Biont_SubPlugins_PluginsView {
 
+	/**
+	 * @var array
+	 */
+	private $installed_plugins = array();
 
-class Biont_SubPlugins_PluginsView
-{
+	/**
+	 * @var array
+	 */
+	private $active_plugins = array();
 
-    private $installed_plugins = array();
+	/**
+	 * @var string
+	 */
+	private $prefix = '';
 
-    private $active_plugins = array();
+	/**
+	 * Setup class variables
+	 *
+	 * @param $installed_plugins
+	 * @param $active_plugins
+	 * @param $prefix
+	 */
+	public function __construct( $installed_plugins, $active_plugins, $prefix ) {
 
-    private $prefix = '';
+		$this->installed_plugins = $installed_plugins;
+		$this->active_plugins    = $active_plugins;
+		$this->prefix            = $prefix;
+	}
 
-    public function __construct($installed_plugins, $active_plugins, $prefix)
-    {
+	/**
+	 * Display the PluginListTable that manages the subplugins
+	 */
+	public function show() {
 
-        $this->installed_plugins = $installed_plugins;
-        $this->active_plugins = $active_plugins;
-        $this->prefix = $prefix;
-    }
+		?>
 
-    public function show()
-    {
+		<div class="wrap">
 
-        ?>
-
-        <div class="wrap">
-
-            <h2><?php echo esc_html(get_admin_page_title()); ?></h2>
-
-            <form id="sub_plugins" method="get">
-                <input type="hidden" name="page" value="<?php echo $_REQUEST['page'] ?>"/>
-                <?php
-                $table = new Biont_SubPlugins_PluginListTable($this->installed_plugins, $this->prefix);
-                $table->prepare_items();
-                $table->display();
-                ?>
-            </form>
-        </div>
-
-
-    <?php
-    }
+			<h2><?php echo esc_html( get_admin_page_title() ); ?></h2>
+			<?php do_action( 'biont_before_subplugin_list' ) ?>
+			<form id="sub_plugins" method="get">
+				<input type="hidden" name="page" value="<?php echo $_REQUEST[ 'page' ] ?>" />
+				<?php
+				do_action( 'biont_subplugin_form_fields' );
+				$table = new Biont_SubPlugins_PluginListTable( $this->installed_plugins, $this->prefix );
+				$table->prepare_items();
+				$table->display();
+				?>
+			</form>
+			<?php do_action( 'biont_after_subplugin_list' ) ?>
+		</div>
+	<?php
+	}
 }
